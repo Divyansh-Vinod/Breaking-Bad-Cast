f10b0d4ec63aa3cb70f60357add616c8a44649df
Divyansh Vinod <divyanshjns@gmail.com>
2022-03-08 01:30:17 +0530

    Updates

---
+++ precache-manifest.df9160aac5e4620780759dc3ffa19793.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eb78cae5d21b3706136a67378454af1",
+    "url": "/Breaking-Bad-Cast/index.html"
+  },
+  {
+    "revision": "232a62b526e9dd678093",
+    "url": "/Breaking-Bad-Cast/static/css/main.3155dc01.chunk.css"
+  },
+  {
+    "revision": "c9413a2b0e288bcb1e85",
+    "url": "/Breaking-Bad-Cast/static/js/2.68f4301c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Breaking-Bad-Cast/static/js/2.68f4301c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "232a62b526e9dd678093",
+    "url": "/Breaking-Bad-Cast/static/js/main.0b7653b0.chunk.js"
+  },
+  {
+    "revision": "a3b6a786245ad3955008",
+    "url": "/Breaking-Bad-Cast/static/js/runtime-main.8015c9c4.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/Breaking-Bad-Cast/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/Breaking-Bad-Cast/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/Breaking-Bad-Cast/static/media/spinner.235b9b3e.gif"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Breaking-Bad-Cast/precache-manifest.df9160aac5e4620780759dc3ffa19793.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Breaking-Bad-Cast/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
